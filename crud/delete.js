bb3359d9263046bbf1d4e2a1d538cb9094ea0a5d
Liscano Natanael <hidromagnetismo@gmail.com>
2021-02-26 07:43:24 +0000

    findByIdAndDelete(), Buscar por _id, retornar y luego eliminar un documento dado el ID

--- crud/delete.js
+++ crud/delete.js
@@ -5,7 +5,7 @@ require('../connection');
 const Product = require('../models/Product');
 
 const someFunction = async () => {
-    const result = await Product.findOneAndDelete({name: 'Keyboard'});
+    const result = await Product.findByIdAndDelete('60389946bf4ecc563953e4da');
     console.log(result);
 }
 
